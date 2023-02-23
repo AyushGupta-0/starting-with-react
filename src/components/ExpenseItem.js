4e11dd92ea5fb529cad6faf9a1b33a34fe347d6a
AyushGupta-0 <118269589+AyushGupta-0@users.noreply.github.com>
2023-02-23 14:15:45 +0530

    adding style to components and using variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,16 @@
-function ExpenseItem(){
-    return <h2>
-        Expense Item !
-    </h2>
+import './ExpenseItem.css'
+function ExpenseItem() {
+    const expenseDate = new Date(2023 , 1, 22);
+    const expenseTitle = ' Car insurance';
+    const expenseAmount = 41.33;
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate.toDateString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
