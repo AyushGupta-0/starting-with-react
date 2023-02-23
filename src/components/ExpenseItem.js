a70ea395f4f1f073affb997a94a1a379deeaf985
AyushGupta-0 <118269589+AyushGupta-0@users.noreply.github.com>
2023-02-23 16:42:15 +0530

    making detail component which has title & amount

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,12 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 function ExpenseItem(props) {
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.date}/>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
+      <ExpenseDetails amount={props.amount} title={props.title}/>
+      
     </div>
   );
 }
