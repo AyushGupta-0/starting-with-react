efbe7b2dd327ff035575681bf48e04ffd85a704b
AyushGupta-0 <118269589+AyushGupta-0@users.noreply.github.com>
2023-02-24 14:13:00 +0530

    changing amount onClick on screen using state.

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,17 +1,24 @@
-import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import ExpenseDetails from './ExpenseDetails';
-const ExpenseItem=(props)=> {
-  const clickHandler =()=>{
-    console.log('clicked!')
-  }
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import React, { useState } from "react";
+const ExpenseItem = (props) => {
+  const [amount, setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setAmount('100')
+    console.log(amount);
+  };
+
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date}/>
-      <ExpenseDetails amount={props.amount} title={props.title}/>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
       <button onClick={clickHandler}>Change title</button>
     </Card>
   );
-}
+};
 export default ExpenseItem;
