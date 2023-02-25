01b2b49c318c8c7a1cc379bf3cf09b3d0aa7712c
Ayush Gupta <118269589+AyushGupta-0@users.noreply.github.com>
2023-02-25 20:29:42 +0530

    writing a msg if the list is empty or have 1 elem

--- Expenses.js
+++ Expenses.js
@@ -4,15 +4,28 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 const Expenses = (props) => {
-
   const [filteredYear, setFilteredYear] = useState("2019");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  
-  const filteredExpenses = props.items.filter(expense=>{
+
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+  let expenseContent = <p>No Expense Found</p>;
+  if(filteredExpenses.length > 0){
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -20,14 +33,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expenseContent}
       </Card>
     </div>
   );
